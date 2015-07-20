df81a6f3057e992d21f98eac7ba256b66f6de279
imittal <imittal@mysite.com>
2015-07-19 22:32:55 -0700

    Reordering functions within cracking-coding-interview/src/sort/QuickSort

--- cracking-interview/src/sort/QuickSort.java
+++ cracking-interview/src/sort/QuickSort.java
@@ -1,49 +1,22 @@
 package sort;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class QuickSort {
-	
-	// Not in-place (in-place would be more efficient)
-	public static void quickSort(ArrayList<Integer> nums) {
-	    if (nums.isEmpty() || nums.size() == 1) {
-	        return;
-	    }
 
-	    Integer pivot = nums.get(0);
-	    ArrayList<Integer> left  = new ArrayList<Integer> ();
-	    ArrayList<Integer> right = new ArrayList<Integer> ();
-
-	    for (Integer i : nums) {
-	        if (i < pivot) {
-	            left.add(i);
-	        }
-	        else {
-	            right.add(i);
-	        }
-	    }
-	    
-	    quickSort(left);
-	    quickSort(right);
-
-	    nums.clear();
-	    nums.addAll(left);
-	    nums.add(pivot);
-	    nums.addAll(right);
-	}
-	
 	// In-place sort
-	public static void sort(int[] values) {
-		sort(values, 0, values.length - 1);
+	public static void quickSort(int[] values) {
+		quickSort(values, 0, values.length - 1);
 	}
 
-	public static void sort(int[] values, int start, int end) {
+	public static void quickSort(int[] values, int start, int end) {
 		if (start > end) {
 			return;
 		}
 		int pivot = values[end];
 		int swapPos = end - 1;
-		
+
 		for (int pos = start; pos < swapPos; pos++) {
 			if (values[pos] > pivot) {
 				swap(values, swapPos, pos);
@@ -53,12 +26,12 @@ public class QuickSort {
 		}
 		if (values[swapPos] >= pivot) {
 			swap(values, swapPos, end);
-			sort(values, start, swapPos - 1);
-			sort(values, swapPos + 1, end);
+			quickSort(values, start, swapPos - 1);
+			quickSort(values, swapPos + 1, end);
 		} else {
 			swap(values, swapPos + 1, end);
-			sort(values, start, swapPos);
-			sort(values, swapPos + 2, end);
+			quickSort(values, start, swapPos);
+			quickSort(values, swapPos + 2, end);
 		}
 	}
 
@@ -66,5 +39,33 @@ public class QuickSort {
 		int temp = values[pos];
 		values[pos] = values[swapPos];
 		values[swapPos] = temp;
+	}
+
+	// Not in-place (in-place would be more efficient)
+	public static void quickSortNotInPlace(List<Integer> nums) {
+		if (nums.isEmpty() || nums.size() == 1) {
+			return;
+		}
+
+		Integer pivot = nums.get(0);
+		List<Integer> left  = new ArrayList<Integer> ();
+		List<Integer> right = new ArrayList<Integer> ();
+
+		for (Integer i : nums) {
+			if (i < pivot) {
+				left.add(i);
+			}
+			else {
+				right.add(i);
+			}
+		}
+
+		quickSortNotInPlace(left);
+		quickSortNotInPlace(right);
+
+		nums.clear();
+		nums.addAll(left);
+		nums.add(pivot);
+		nums.addAll(right);
 	}
 }
