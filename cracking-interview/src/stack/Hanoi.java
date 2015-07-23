5ee216afdb1ab49e52628b372334cab406801820
imittal <imittal@mysite.com>
2015-07-22 22:30:09 -0700

    Cleaning up Hanoi in cracking - stack

--- cracking-interview/src/stack/Hanoi.java
+++ cracking-interview/src/stack/Hanoi.java
@@ -4,11 +4,7 @@ import java.util.Stack;
 
 public class Hanoi<T> {
 	void move(Stack<T> from, Stack<T> to, Stack<T> using, int numPlates) {
-		if (numPlates == 0 || from.size() == 0) {
-			return;
-		}
-		if (numPlates == 1) {
-			to.push(from.pop());
+		if (numPlates <= 0) {
 			return;
 		}
 		move(from, using, to, numPlates - 1);
