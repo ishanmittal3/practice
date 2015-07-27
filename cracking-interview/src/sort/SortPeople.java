a10c61363a4a02f9c3f324f2874569e16bbf027b
unknown <imittal@WS1587PA.theranos.local>
2015-07-26 19:34:10 -0700

    Adding SortPeople to cracking/sort

--- cracking-interview/src/sort/SortPeople.java
+++ cracking-interview/src/sort/SortPeople.java
@@ -52,7 +52,7 @@ public class SortPeople {
 	public static void main(String[] args) {
 		List<Person> persons = new ArrayList<Person>();
 		persons.add(new Person(65, 100));
-		persons.add(new Person(70, 150));
+		persons.add(new Person(70, 100));
 		persons.add(new Person(56, 90));
 		persons.add(new Person(75, 190));
 		persons.add(new Person(60, 95));
