214df5ed48b467d94f9deb7e3e3ce1010d2718c9
imittal <imittal@mysite.com>
2015-07-31 23:12:05 -0700

    Updating BstClosestNode in sandbox

--- sandbox/src/sandbox/BstClosestNode.java
+++ sandbox/src/sandbox/BstClosestNode.java
@@ -2,5 +2,29 @@ package sandbox;
 
 public class BstClosestNode {
 
+	Node closestNode(Node node, int key) {
+		return closestNode(node, key, null);
+	}
 	
+	Node closestNode(Node node, int key, Node closest) {
+		if(node == null) {
+			return closest;
+		}
+		if(node.value == key) {
+			return node;
+		}
+		if(closest == null || 
+				Math.abs(node.value - key) < Math.abs(closest.value - key)) {
+			closest = node;
+		}
+		Node next = node.value < key ? 
+				node.right : node.left;
+		return closestNode(next, key, closest);
+	}
+	
+	static class Node {
+		int value;
+		Node left;
+		Node right;
+	}
 }
