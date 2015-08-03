4ceb474e4f71c5d366ae721c08478efbc568b71c
imittal <imittal@mysite.com>
2015-08-02 23:36:18 -0700

    Adding alternate solution to LongestCommonSubstring in dp sandbox

--- sandbox/src/dp/LongestCommonSubstring.java
+++ sandbox/src/dp/LongestCommonSubstring.java
@@ -16,6 +16,24 @@ public class LongestCommonSubstring {
 			str2 = temp;
 		}
 		int max = 0;
+		/*int[][] lengths = new int[str1.length()][str2.length()];
+		*//**
+		 * lengths[i][j] stores the length of the 
+		 * longest substring ending at position i in str1
+		 * and j in str2
+		 *//*
+		
+		for(int pos1 = 0; pos1 < str1.length(); pos1++) {
+			for(int pos2 = 0; pos2 < str2.length(); pos2++) {
+				if(str1.charAt(pos1) != str2.charAt(pos2)) {
+					lengths[pos1][pos2] = 0;
+				} else {
+					lengths[pos1][pos2] = (pos1 == 0 || pos2 == 0) ?
+							1 : 1 + lengths[pos1 - 1][pos2 - 1];
+					max = Math.max(max, lengths[pos1][pos2]);
+				}
+			}
+		}*/		
 		int[] curr = new int[str1.length()];
 		int[] prev = new int[str1.length()];
 		/**
