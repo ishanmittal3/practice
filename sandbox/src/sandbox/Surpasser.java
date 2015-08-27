eaa6de2a9e7637072164da6bbf8a67afe6849e5f
Ishan Mittal <imittal@theranos.com>
2015-08-26 19:07:18 -0700

    Adding Surpasser to sandbox

---
+++ sandbox/src/sandbox/Surpasser.java
@@ -0,0 +1,28 @@
+package sandbox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Surpasser {
+
+	/**
+	 * For each number in an array, find the first number
+	 * to its right that is greater than this number
+	 * If no such number exists, assign -1 
+	 */
+	List<Pair> getSurpassers(int[] nums) {
+		Stack<Integer> stack = new Stack<Integer>();
+		List<Pair> result = new ArrayList<Pair>();
+		for(int num : nums) {
+			while(!stack.isEmpty() && num > stack.peek()) {
+				result.add(new Pair(stack.pop(), num));
+			}
+			stack.push(num);
+		}
+		while(!stack.isEmpty()) {
+			result.add(new Pair(stack.pop(), -1));
+		}
+		return result;
+	}
+}
