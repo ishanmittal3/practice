5dffc865f70f20d01d060bf052d4698769f4f607
unknown <imittal@WS1587PA.theranos.local>
2015-09-14 15:22:55 -0700

    Adding ReverseInRange to program-creek/linkedList

--- program-creek/src/linkedList/ReverseInRange.java
+++ program-creek/src/linkedList/ReverseInRange.java
@@ -2,11 +2,34 @@ package linkedList;
 
 public class ReverseInRange {
 
-	void reverse(Node head, int start, int end) {
+	Node reverse(Node head, int start, int end) {
 		Node curr = head;
+		Node prev = start > 0 ? head : null;
 		int pos = 0;
-		while(pos < start) {
-			curr = curr.next;
+		while(pos < start - 1) {
+			prev = prev.next;
+			pos++;
+		}
+		if(prev != null) {
+			curr = prev.next;			
+		}
+		Node prevStartNode = prev;
+		Node startNode = curr;
+		
+		prev = curr;
+		curr = curr.next;
+		for(pos = start; pos < end; pos++) {
+			Node next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		startNode.next = curr;
+		if(prevStartNode != null) {
+			prevStartNode.next = prev;
+			return head;
+		} else {
+			return prev;
 		}
 	}
 }
