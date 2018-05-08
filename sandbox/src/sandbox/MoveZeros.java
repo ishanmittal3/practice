b7ada07d3947544044558d9befaab1dc5ce20aa2
Ishan Mittal <ishan_mittal@apple.com>
2018-05-08 13:37:30 -0700

    Adding test case to MoveZeros

--- sandbox/src/sandbox/MoveZeros.java
+++ sandbox/src/sandbox/MoveZeros.java
@@ -1,32 +1,29 @@
 package sandbox;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class MoveZeros {
 
-	void moveZeros(int[] nums) {
-		int pos1 = 0;
-		int pos2 = 0;
+    public static void main(String[] args) {
+        int[] nums = new int[] {0, -1, 4, 0, 5, 7};
+        moveZeros(nums);
+        System.out.println(
+            Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.toList()));
+    }
 
-		while(pos2 < nums.length) {
-			if(nums[pos1] == 0) {
-				if(nums[pos2] == 0) {
-					pos2++;
-				} else {
-					nums[pos1++] = nums[pos2++];
-				}
-			} else {
-				pos1++;
-				pos2++;
-			}
-		}
-
-		for(pos2 = 0; pos2 < nums.length; pos2++) {
-			if(nums[pos1] == 0) {
-				if(nums[pos2] != 0) {
-					nums[pos1++] = nums[pos2];
-				}
-			} else {
-				pos1++;
-			}
-		}
+	private static void moveZeros(int[] nums) {
+		int nonZero = 0;
+		for (int pos = 0; pos < nums.length; pos++) {
+		    if (nums[pos] != 0) {
+		        nums[nonZero++] = nums[pos];
+            }
+        }
+        int pos = nonZero;
+		while (pos < nums.length) {
+		    nums[pos++] = 0;
+        }
 	}
 }
